ff83f8a90d5e3f745c37e51f58a385e2af938600
maps_kelvin_souza <kelvin.souza@maps.com.br>
2022-12-27 16:37:55 -0300

    enviando email caso o consumo de memoria bata 50%

--- scripts/verificacao-memoria-consumida.sh
+++ scripts/verificacao-memoria-consumida.sh
@@ -5,4 +5,9 @@ memoria_consumida=$(free | grep -i mem | awk '{ print $3 }')
 
 relacao_memoria_atual_total=$(bc <<< "scale=2;$memoria_consumida/$memoria_total * 100" | awk -F. '{ print $1 }')
 
-echo $relacao_memoria_atual_total 
+if [ $relacao_memoria_atual_total -gt 50 ]
+then
+	mail-s "Consumo de memoria acima do limite" kelvinlimasouza226@gmail.com<<del 
+O consumo de memória esta acima do limite que foi especificado. atualmente o consumo é de $(free -h | grep -i mem | awk '{ print $3 }')
+del
+fi
