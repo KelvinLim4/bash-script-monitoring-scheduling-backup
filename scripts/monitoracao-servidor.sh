7cafe5610b387dbbbcfa25148c9de699541be94a
maps_kelvin_souza <kelvin.souza@maps.com.br>
2022-12-27 14:06:35 -0300

    Melhorando script e enviando email caso o servidor fique indisponivel

--- scripts/monitoracao-servidor.sh
+++ scripts/monitoracao-servidor.sh
@@ -3,4 +3,10 @@
 
 resposta_http=$(curl --write-out %{http_code} --silent  --output /dev/null http://localhost)
 
-echo $resposta_http
+if [ $resposta_http -ne 200 ]
+then 
+mail -s "Problema no servidor" kelvinlimasouza226@gmail.com<<del
+Houve um problema no servidor apache e os usuários pararam de ter acesso ao conteudo web.
+del
+	systemctl restart apache2
+fi
